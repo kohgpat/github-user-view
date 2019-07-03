9532b2186b5cce289d9cc00274525ae1c2e5a5c9
Nikolay Burlov <kohgpat@gmail.com>
2019-07-03 15:35:33 +0800

    Implement simple landing

--- src/screens/Landing/index.js
+++ src/screens/Landing/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Container, Heading, Hero, Button } from "react-bulma-components";
 
 export default function Landing() {
   return (
     <div>
-      Landing
+      <Hero color="warning" size="fullheight">
+        <Hero.Body>
+          <Container>
+            <Heading size={1}>Github User View</Heading>
+            <Heading size={4}>
+              React demo application - Github user viewer
+            </Heading>
+            <Button color="black">Login with Github</Button>
+          </Container>
+        </Hero.Body>
+      </Hero>
     </div>
   );
 }
